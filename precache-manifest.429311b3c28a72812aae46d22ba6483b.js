cb9b0f7a433831a19b992a281a974004f4c60978
Earle-Poole <eepoole4@gmail.com>
2018-10-30 16:12:51 -0500

    Updates

---
+++ precache-manifest.429311b3c28a72812aae46d22ba6483b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2752ea3d79955261b07c",
+    "url": "/coin-stock-info/static/js/runtime~main.2752ea3d.js"
+  },
+  {
+    "revision": "87fd86312841ac84c732",
+    "url": "/coin-stock-info/static/js/main.87fd8631.chunk.js"
+  },
+  {
+    "revision": "a55c4992d1c0969a1992",
+    "url": "/coin-stock-info/static/js/1.a55c4992.chunk.js"
+  },
+  {
+    "revision": "87fd86312841ac84c732",
+    "url": "/coin-stock-info/static/css/main.482d3a3a.chunk.css"
+  },
+  {
+    "revision": "a55c4992d1c0969a1992",
+    "url": "/coin-stock-info/static/css/1.351008ef.chunk.css"
+  },
+  {
+    "revision": "48abf1fd21333bcc21225f99e7dc71eb",
+    "url": "/coin-stock-info/index.html"
+  }
+];
